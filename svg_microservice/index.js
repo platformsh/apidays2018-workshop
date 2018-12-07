b8c2931115c04f9253b83464708bc0f271a1c784
Ori Pekelman <ori@platform.sh>
2018-12-07 17:30:07 +0000

    cleanup node app

--- svg_microservice/index.js
+++ svg_microservice/index.js
@@ -68,11 +68,7 @@ var server = http.createServer(function (request, response) {
             }
             response.end(JSON.stringify(data))
         }
-        else {
-            response.writeHead(200, {"Content-Type": "text/html"});
-            response.end("<html><head><title>Hello node</title></head><body><h1><img src='public/js.png'>Hello Node</h1><h3>Platform configuration:</h3><pre>"+JSON.stringify(config, null, 4) + "</pre></body></html>");
-        }
-	}
+    }
 });
 
 server.listen(config.port||8080);
