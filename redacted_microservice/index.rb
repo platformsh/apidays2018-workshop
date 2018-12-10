59ae77cf8089df114ec29844373bd7865740982c
Ludovico Altana <ludovico@platform.sh>
2018-12-10 17:23:00 +0100

    log ruby service

--- redacted_microservice/index.rb
+++ redacted_microservice/index.rb
@@ -16,6 +16,13 @@ end
 
 module Text
   class API < Grape::API
+    helpers do
+      def logger
+        API.logger
+      end
+    end
+    self.logger.level = Logger::INFO
+
     version 'v1', using: :header, vendor: 'platform_sh'
     content_type :txt, 'text/plain'
     content_type :json, 'application/json'
@@ -27,18 +34,21 @@ module Text
 
     desc "Returns redacted text"
     get "" do
+      logger.info "\"#{params[:text]}\" received to redact"
       content_type 'text/plain'
       redact(params[:text])
     end
     
     desc 'Returns redacted text'
     post "" do
+      logger.info "\"#{params[:text]}\" received to redact"
       content_type 'text/plain'
       redact(params[:text])
     end
 
     desc 'Discovery'
     get "discover" do
+      logger.info "Received a discovery request"
       { "name"=>"redacted", "type"=>"*ast.Text", "flags"=>{ "composable"=>true } }
     end
   end
